a80edd850920b2ce1dbef65fedbcea3488aa2026
yutanim <yuta.tanimura425@gmail.com>
2017-02-07 23:44:42 +0900

    映像にtextFieldを埋めこむようにした

--- moviecam/MoviePreviewVC.swift
+++ moviecam/MoviePreviewVC.swift
@@ -9,11 +9,20 @@
 import UIKit
 import Rswift
 import AVFoundation
+import GPUImage
 
 class MoviePreviewViewController: UIViewController {
     var moviePath: URL?
-    var player : AVPlayer?
-    @IBOutlet weak var previewView: UIView!
+    var player : GPUImageMovie!
+    @IBOutlet weak var previewView: GPUImageView!
+    @IBOutlet weak var textButton: UIButton!
+    var filter:GPUImageFilter!
+    var blendFilter: GPUImageAlphaBlendFilter!
+    var textInput : GPUImageUIElement?
+    var vi: GPUImageUIElement?
+    var textField :UITextField!
+    
+    
     
     class func instantiate(path: String) -> MoviePreviewViewController {
         guard let vc =  R.storyboard.moviePreviewViewController.instantiateInitialViewController() else {
@@ -28,22 +37,47 @@ class MoviePreviewViewController: UIViewController {
         guard let url = moviePath else {
             return
         }
+        player = GPUImageMovie.init(url: url)
+        filter = GPUImageBilateralFilter()
+        blendFilter = GPUImageAlphaBlendFilter()
+        blendFilter.mix = 1.0
+        player.addTarget(filter)
         let item = AVPlayerItem(url: url)
         NotificationCenter.default.addObserver(self, selector: #selector(MoviePreviewViewController.playerDidFinished), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object:item)
-        player = AVPlayer(playerItem: item)
-        let playerLayer = AVPlayerLayer(player: player)
-        playerLayer.frame = CGRect(x: 0, y: 0, width: self.previewView.frame.width, height: self.previewView.frame.height)
-        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-        self.previewView.layer.addSublayer(playerLayer)
-        self.player?.play()
+        vi = GPUImageUIElement(view: UIView.init())
+        player.playAtActualSpeed = true
+        player.shouldRepeat = true
+        filter = GPUImageFilter.init()
+        player.addTarget(filter)
+        filter.addTarget(blendFilter)
+        vi?.addTarget(blendFilter)
+        blendFilter.addTarget(previewView)
+        textField = UITextField.init(frame: self.previewView.frame)
+        textInput = GPUImageUIElement(view: textField)
+        textInput?.addTarget(blendFilter)
+        filter.frameProcessingCompletionBlock = { _ , _ in
+            self.vi?.update()
+            self.textInput?.update()
+        }
+        player.startProcessing()
     }
+    
+    @IBAction func textButtonDidTouchDown(_ sender: UIButton) {
+        if textField?.isHidden ?? true {
+            textField?.isHidden = false
+            self.previewView.bringSubview(toFront: textField!)
+        } else {
+            textField?.isHidden = true
+        }
+    }
+    
     
     @IBAction func closeButtonDidTouchUpInside(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
     func playerDidFinished(_: Notification) {
-        self.player?.play()
+        
     }
 
 }
